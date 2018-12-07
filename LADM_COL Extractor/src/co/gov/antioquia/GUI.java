8d3f3c81a5174c204b369cf669705eaa36bfa66f
jslobo <juan.lobo@udea.edu.co>
2018-12-07 17:21:31 -0500

    GUI layout set up

--- LADM_COL Extractor/src/co/gov/antioquia/GUI.java
+++ LADM_COL Extractor/src/co/gov/antioquia/GUI.java
@@ -12,6 +12,8 @@ import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import javax.swing.JSeparator;
+import java.awt.Toolkit;
 
 public class GUI {
 
@@ -49,53 +51,72 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\jlobor\\My Documents\\eclipse-workspace\\LADM_COL-Extractor\\Sources\\LogoCatastro.ico"));
+		frame.setBounds(100, 100, 397, 246);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		
 		JPanel panel = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-		flowLayout.setAlignment(FlowLayout.LEFT);
 		tabbedPane.addTab("Conexión", null, panel, null);
+		panel.setLayout(null);
 		
 		JLabel lblNombreDeConexin = new JLabel("Nombre de conexión");
+		lblNombreDeConexin.setBounds(5, 8, 121, 14);
 		lblNombreDeConexin.setVerticalAlignment(SwingConstants.TOP);
 		panel.add(lblNombreDeConexin);
 		
 		textField = new JTextField();
+		textField.setBounds(136, 5, 237, 20);
 		panel.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(5, 33, 98, 14);
 		panel.add(lblUsuario);
 		
 		textField_1 = new JTextField();
+		textField_1.setBounds(136, 30, 237, 20);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblContrasea = new JLabel("Contraseña");
+		lblContrasea.setBounds(5, 58, 98, 14);
 		panel.add(lblContrasea);
 		
 		passwordField = new JPasswordField();
+		passwordField.setBounds(136, 55, 237, 20);
 		panel.add(passwordField);
 		
 		JLabel lblNombreDeTabla = new JLabel("Nombre de BD");
+		lblNombreDeTabla.setBounds(5, 83, 98, 14);
 		panel.add(lblNombreDeTabla);
 		
 		textField_2 = new JTextField();
+		textField_2.setBounds(136, 80, 237, 20);
 		panel.add(textField_2);
 		textField_2.setColumns(10);
 		
 		JButton btnProbarConexin = new JButton("Probar conexión");
+		btnProbarConexin.setBounds(5, 128, 133, 23);
 		panel.add(btnProbarConexin);
 		
 		JButton btnDesconectar = new JButton("Desconectar");
+		btnDesconectar.setBounds(141, 128, 120, 23);
 		panel.add(btnDesconectar);
 		
 		JButton btnConectar = new JButton("Conectar");
+		btnConectar.setBounds(264, 128, 109, 23);
 		panel.add(btnConectar);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(5, 111, 368, 2);
+		panel.add(separator);
+		
+		JLabel lblNewLabel = new JLabel("Estado:");
+		lblNewLabel.setBounds(25, 155, 46, 14);
+		panel.add(lblNewLabel);
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Reporte de Actualización Catastral", null, panel_1, null);
@@ -103,5 +124,4 @@ public class GUI {
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("Ayuda", null, panel_2, null);
 	}
-
 }
